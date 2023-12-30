d264ff76486ffbc889f2dec038c1f4b6f23740b3
yangrui9501 <yang77567789@gmail.com>
2023-12-30 17:49:52 +0800

    upload project

---
+++ README.md
@@ -0,0 +1,10 @@
+# CMake_Project_Template
+
+- 創建：May 31, 2023
+- 最後一次更新：2023-10-02
+  
+## Log
+
+- 在 windows 環境好像要編譯兩次?
+- 在 linux 不用，等下回 windows 測試
+- 看起來 linux 好像會自動往下找子目錄當中的動態程式庫
---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+/**
+ * @file main.cpp
+ * @author Yang-Rui Li (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-12-30
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <ctime>
+
+int main() {
+    int start, end;
+    
+    while (true) {
+        std::cout << "Enter the start and end of the range (e.g., 1 18): ";
+        std::cin >> start >> end; 
+
+        if (std::cin.fail()) {
+            std::cout << "Input Error. Please enter integers (e.g., 1 18)\n";
+            // Clear error flag
+            std::cin.clear();
+            // Ignore wrong input
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            break;
+        }
+    }
+    std::cout << "The range of draw is: (" << start << ", " << end << ")\n";
+
+    std::vector<int> numbers;
+    for (int i = start; i <= end; ++i) {
+        numbers.push_back(i);
+    }
+
+    std::mt19937 rng(std::time(nullptr)); // random number generator
+    std::shuffle(numbers.begin(), numbers.end(), rng);
+
+    char continueDrawing = 'y';
+    while (!numbers.empty()) {
+        // If the user enters 'y' or 'Y', draw a number
+        if (continueDrawing == 'y' || continueDrawing == 'Y') {
+            std::cout << "Drawn number: " << numbers.back() << std::endl;
+            numbers.pop_back();
+        }
+
+        if (!numbers.empty()) {
+            do {
+                std::cout << "Do you want to draw another number? (y/n): ";
+                std::cin >> continueDrawing;
+
+                // Check if the input is valid
+                if (continueDrawing != 'y' && continueDrawing != 'Y' && 
+                    continueDrawing != 'n' && continueDrawing != 'N') {
+                    std::cout << "Invalid input. Please enter 'y' or 'n'.\n";
+                }
+            } while (continueDrawing != 'y' && continueDrawing != 'Y' && 
+                     continueDrawing != 'n' && continueDrawing != 'N');
+        }
+
+        if (continueDrawing == 'n' || continueDrawing == 'N') {
+            break;  // If the user enters 'n' or 'N', exit the loop
+        }
+    }
+
+    return 0;
+}
